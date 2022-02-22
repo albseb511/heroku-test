09106a2664c29c5a2f26c91fb20ff94cc5ebe068
albseb511 <albertsebastian511@gmail.com>
2022-02-22 18:58:02 +0530

    project init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const port = process.env.PORT || 3000
+const app = express()
+const cors = require("cors");
+
+app.use(cors())
+// setup cors
+app.get("/", (req, res) =>{
+    res.send("Hello World")
+})
+
+// listen to port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+    }
+)
